bb440864ccc8c81ae2a25bf072a8970808fcab3f
Panu Simolin <p.simolin@gmail.com>
2024-12-04 13:20:47 +0200

    Learn go arrays and functions

--- day_01/office.go
+++ day_01/office.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type inputs [7]string
+
+// is this a comment
+func get_input(filep string) inputs {
+	f, err := os.Open(filep)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// defer so that we close the file always
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var input_lines inputs
+	i := 0
+	for scanner.Scan() {
+		input_lines[i] = scanner.Text()
+		i++
+	}
+
+	return input_lines
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+	i_lines := get_input("test-input-2.txt")
+	for i := 0; i < 7; i++ {
+		fmt.Println(i_lines[i])
+	}
 }
