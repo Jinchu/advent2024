78f72dfcb53a42769975959b3d742557c34fcb33
Panu Simolin <p.simolin@gmail.com>
2024-12-09 16:25:00 +0200

    use pointers

--- day_01/office.go
+++ day_01/office.go
@@ -8,9 +8,10 @@ import (
 )
 
 type inputs []string
+type inputs_arr [8]string
 
 // is this a comment
-func get_input(filep string) inputs {
+func get_input(input_lines inputs, filep string) inputs {
 	f, err := os.Open(filep)
 	if err != nil {
 		log.Fatal(err)
@@ -20,7 +21,6 @@ func get_input(filep string) inputs {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
-	var input_lines inputs
 	for scanner.Scan() {
 		input_lines = append(input_lines, scanner.Text())
 	}
@@ -28,14 +28,43 @@ func get_input(filep string) inputs {
 	return input_lines
 }
 
+func get_input_to_array(inputs_arr *inputs_arr, filep string) {
+	f, err := os.Open(filep)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// defer so that we close the file always
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	i := 0
+	for scanner.Scan() {
+
+		inputs_arr[i] = scanner.Text()
+		i++
+	}
+
+	return
+}
+
 func main() {
 	fmt.Println("Hello, World!")
-	input_lines := get_input("test-input-2.txt")
+	var input_lines inputs
+	var input_array inputs_arr
+	input_lines = get_input(input_lines, "test-input-2.txt")
 	fmt.Printf("i_lines type %T\n", input_lines)
 	// 	fmt.Println(i_lines)
+
+	get_input_to_array(&input_array, "test-input-2.txt")
 
 	for i, v := range input_lines {
 
 		fmt.Printf("line %v:         %v\n", i, v)
+	}
+
+	fmt.Println("----")
+	for _, v := range input_array {
+		fmt.Printf("line: %v\n", v)
 	}
 }
