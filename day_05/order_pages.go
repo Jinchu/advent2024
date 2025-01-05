986fcd9d6fc3a55af5d2a2a62d2f8507fc3d2fd1
Panu Simolin <p.simolin@gmail.com>
2025-01-05 10:53:40 +0200

    Implement checks

--- day_05/order_pages.go
+++ day_05/order_pages.go
@@ -13,16 +13,79 @@ type rule struct {
 }
 
 func PrintOrder() {
+	debug := false
 	// inputLines := input.GetInputV2("./day_05/test-input-1.txt")
 	//	inputLines := input.GetInputV2("./day_05/input-day5.txt")
 
 	rules, pageNumbers := getRulesAndPages("./day_05/test-input-1.txt")
-	for _, update := range pageNumbers {
-		fmt.Println(update[2])
-		if rules[2].before == 97 {
-			fmt.Println("dummy")
+
+	if debug {
+		for _, update := range pageNumbers {
+			fmt.Println(update[2])
+			if rules[2].before == 97 {
+				fmt.Println("dummy")
+			}
 		}
 	}
+
+	for _, update := range pageNumbers {
+		isValid := checkUpdate(update, rules)
+		if isValid {
+			fmt.Printf("Valid update found %v\n", update)
+		}
+	}
+}
+
+func checkUpdate(wholeUpdate []int, ruleBook []rule) bool {
+	for _, currentRule := range ruleBook {
+		for i := range wholeUpdate {
+			if !checkRight(wholeUpdate, currentRule, i) {
+				return false
+			}
+			if !checkLeft(wholeUpdate, currentRule, i) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func checkLeft(wholeUpdate []int, currentRule rule, checkIndex int) bool {
+	if currentRule.before != wholeUpdate[checkIndex] {
+		return true // rule is not applicable
+	}
+
+	for i := checkIndex - 1; i > 0; i-- {
+		var checkNumber int
+		checkNumber = wholeUpdate[i]
+
+		if checkNumber == currentRule.after {
+			fmt.Printf("Left check: line %v breaks the rule %v\n", wholeUpdate, currentRule)
+			fmt.Printf("Current page number [%v] %v\n\n", checkIndex, checkNumber)
+			return false
+		}
+	}
+
+	return true // no rule braking numbers found
+
+}
+
+func checkRight(wholeUpdate []int, currentRule rule, checkIndex int) bool {
+	if currentRule.after != wholeUpdate[checkIndex] {
+		return true // rule is not applicable
+	}
+
+	for i := checkIndex + 1; i < len(wholeUpdate); i++ {
+		var checkNumber int
+		checkNumber = wholeUpdate[i]
+
+		if checkNumber == currentRule.before {
+			fmt.Printf("Right check: line %v breaks the rule %v\n", wholeUpdate, currentRule)
+			return false
+		}
+	}
+
+	return true // no rule braking numbers found
 }
 
 func convertUpdateNumbers(updateInput string) []int {
@@ -52,13 +115,13 @@ func getRulesAndPages(fileName string) ([]rule, [][]int) {
 			parts := strings.Split(l, "|")
 			beforeInt, err := strconv.Atoi(parts[0])
 			if err != nil {
-				fmt.Printf("Cannot convert %v to integer", parts[0])
+				fmt.Printf("\nERROR: Cannot convert %v to integer\n", parts[0])
 				panic(err)
 			}
 
 			afterInt, err := strconv.Atoi(parts[1])
 			if err != nil {
-				fmt.Printf("Cannot convert %v to integer", parts[1])
+				fmt.Printf("\nERROR: Cannot convert %v to integer\n", parts[1])
 				panic(err)
 			}
 
